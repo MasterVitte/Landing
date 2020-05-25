1bb549dd58a9bd229f7546376920facd07b2dff0
Artem Vitte <vitte.st13@gmail.com>
2020-05-26 02:49:41 +0400

    Добавлен slick-slider + верстка третьей секции

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.app_section_programs_content_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="programs_slider_prev_arrow"></div>',
+    nextArrow: '<div class="programs_slider_next_arrow"></div>',
+  });
+});
