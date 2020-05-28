04c34b8b8d3df1bb05d0696bb16b52dfebc2fd9d
Artem Vitte <vitte.st13@gmail.com>
2020-05-28 14:04:47 +0400

    Добавление анимации хедеру и кликабельных ссылок в нем

--- js/main.js
+++ js/main.js
@@ -1,5 +1,89 @@
 $(document).ready(function() {
 
+  // Фиксация хедера
+  var header = $(".app_section_navigation");
+  var scrollPrev = 0;
+
+  $(window).scroll(function() {
+    var scrolled = $(window).scrollTop(); // Высота скролла в px
+    var firstScrollUp = false; // Параметр начала сколла вверх
+    var firstScrollDown = false; // Параметр начала сколла вниз
+
+    // Если скроллим
+    if ( scrolled > 0 ) {
+      // Если текущее значение скролла > предыдущего, т.е. скроллим вниз
+      if ( scrolled > scrollPrev ) {
+        firstScrollUp = false; // Обнуляем параметр начала скролла вверх
+        // Если меню видно
+        if ( scrolled < header.height() + header.offset().top ) {
+          // Если только начали скроллить вниз
+          if ( firstScrollDown === false ) {
+            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+            header.css({
+              "top": topPosition + "px"
+            });
+            firstScrollDown = true;
+          }
+          // Позиционируем меню абсолютно
+          header.css({
+            "position": "absolute"
+          });
+          // Если меню НЕ видно
+        } else {
+          // Позиционируем меню фиксированно вне экрана
+          header.css({
+            "position": "fixed",
+            "top": "-" + header.height() + "px"
+          });
+        }
+
+        // Если текущее значение скролла < предыдущего, т.е. скроллим вверх
+      } else {
+        firstScrollDown = false; // Обнуляем параметр начала скролла вниз
+        // Если меню не видно
+        if ( scrolled > header.offset().top ) {
+          // Если только начали скроллить вверх
+          if ( firstScrollUp === false ) {
+            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+            header.css({
+              "top": topPosition + "px"
+            });
+            firstScrollUp = true;
+          }
+          // Позиционируем меню абсолютно
+          header.css({
+            "position": "absolute"
+          });
+        } else {
+          // Убираем все стили
+          header.removeAttr("style");
+        }
+      }
+      // Присваеваем текущее значение скролла предыдущему
+      scrollPrev = scrolled;
+    }
+
+  });
+
+  // Плавная прокрутка до секции
+  $('.link').click(function() { // тут пишите условия, для всех ссылок или для конкретных
+
+    var firstLink = false;
+
+    if ( $(this).attr("href") === '#about' ) {
+      firstLink = true;
+    }
+
+    $("html, body").animate({
+      scrollTop: firstLink ? 0 : $( $(this).attr("href") ).offset().top + "px" // .top+margin - ставьте минус, если хотите увеличить отступ
+    }, {
+      duration: 1000, // тут можно контролировать скорость
+      easing: "swing"
+    });
+    return false;
+
+  });
+
   // Слайдер секции программы
   $('.app_section_programs_content_slider').slick({
     slidesToShow: 1,
@@ -25,7 +109,6 @@ $(document).ready(function() {
   // Аккордион
   $('.app_section_license_content_rows_row_1_accordion_item_head').on('click', function() {
     let id = $(this).data('id');
-    console.log(id);
     $(this).children('.app_section_license_content_rows_row_1_accordion_item_head_toggle').children('img').toggleClass('accordion_toggle');
     $('.app_section_license_content_rows_row_1_accordion_item_content[data-id='+id+']').toggleClass('accordion_toggle_content');
   });
