8ceb8ff0913321d1481c581c502e099644e5d6b6
Artem Vitte <vitte.st13@gmail.com>
2020-06-03 18:34:49 +0400

    Landing (adaptive)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  // Бургер-кнопка
+  $( ".checkbox1" ).on("change", function() {
+    $('.app_nav_links_box').toggleClass('menu_active');
+  });
+
+  function closeMobileMenu() {
+    $('.checkbox1').prop('checked', false);
+    $('.app_nav_links_box').removeClass('menu_active');
+  };
+
+  $(document).click(function(event) {
+    if ($(event.target).closest('.app_section_navigation').length) return;
+    closeMobileMenu();
+    event.stopPropagation();
+  });
+
+
   // Фиксация хедера
   var header = $(".app_section_navigation");
   var scrollPrev = 0;
@@ -11,62 +28,65 @@ $(document).ready(function() {
 
     // Если скроллим
     if ( scrolled > 0 ) {
+
       // Если текущее значение скролла > предыдущего, т.е. скроллим вниз
       if ( scrolled > scrollPrev ) {
         firstScrollUp = false; // Обнуляем параметр начала скролла вверх
+
         // Если меню видно
         if ( scrolled < header.height() + header.offset().top ) {
+
           // Если только начали скроллить вниз
           if ( firstScrollDown === false ) {
-            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-            header.css({
-              "top": topPosition + "px"
-            });
             firstScrollDown = true;
           }
-          // Позиционируем меню абсолютно
-          header.css({
-            "position": "absolute"
-          });
-          // Если меню НЕ видно
-        } else {
-          // Позиционируем меню фиксированно вне экрана
-          header.css({
-            "position": "fixed",
-            "top": "-" + header.height() + "px"
-          });
+
+          if ( !header.hasClass('navigation_hidden') ) {
+            header.addClass('navigation_hidden');
+          }
+
+          closeMobileMenu();
+
         }
 
         // Если текущее значение скролла < предыдущего, т.е. скроллим вверх
       } else {
+
         firstScrollDown = false; // Обнуляем параметр начала скролла вниз
+
         // Если меню не видно
         if ( scrolled > header.offset().top ) {
+
           // Если только начали скроллить вверх
           if ( firstScrollUp === false ) {
-            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-            header.css({
-              "top": topPosition + "px"
-            });
+
+            if ( !header.hasClass('navigation_hidden') ) {
+              header.addClass('navigation_hidden');
+            }
+
             firstScrollUp = true;
           }
-          // Позиционируем меню абсолютно
-          header.css({
-            "position": "absolute"
-          });
-        } else {
-          // Убираем все стили
-          header.removeAttr("style");
+
+          if ( header.hasClass('navigation_hidden') ) {
+            header.removeClass('navigation_hidden');
+          }
+
+          closeMobileMenu();
+
         }
       }
+
       // Присваеваем текущее значение скролла предыдущему
       scrollPrev = scrolled;
     }
 
   });
 
+
   // Плавная прокрутка до секции
   $('.link').click(function() { // тут пишите условия, для всех ссылок или для конкретных
+
+    closeMobileMenu();
 
     var headerOffset = 0;
 
@@ -85,27 +105,109 @@ $(document).ready(function() {
 
   });
 
+
   // Слайдер секции программы
   $('.app_section_programs_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: '<div class="programs_slider_prev_arrow prev_arrow"></div>',
     nextArrow: '<div class="programs_slider_next_arrow next_arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
   });
+
+
+  // Навигация по цифрам слайдера в секции программы
+  $('.app_section_programs_list ul li').on('click', function() {
+
+    let slide = $(this).data('slide');
+
+    $('.app_section_programs_list ul li').map((item) => {
+      if ( item !== slide ) {
+        $($('.app_section_programs_list ul li')[item]).removeClass('app_section_programs_active_slide');
+      }
+    });
+
+    $(this).addClass('app_section_programs_active_slide');
+    $('.app_section_programs_slider').slick('slickGoTo', slide);
+  });
+
+  $($('.app_section_programs_list ul li:first-child')[0]).trigger('click');
+
+  // Синхронизация циферок с активным слайдом
+  $('.app_section_programs_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+
+    $('.app_section_programs_list ul li').map((item) => {
+      if ( item === nextSlide ) {
+        $($('.app_section_programs_list ul li')[item]).addClass('app_section_programs_active_slide');
+      } else {
+        $($('.app_section_programs_list ul li')[item]).removeClass('app_section_programs_active_slide');
+      }
+    });
+
+  });
+
 
   // Слайдер секции программы курса
   $('.app_section_courses_slider_for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    asNavFor: '.app_section_courses_slider_nav'
+    infinite: false,
+    asNavFor: '.app_section_courses_slider_nav',
+    responsive: [
+      {
+        breakpoint: 1160,
+        settings: {
+          infinite: true,
+        }
+      }
+    ]
   });
 
   $('.app_section_courses_slider_nav').slick({
-    slidesToShow: 9,
-    slidesToScroll: 1,
+    slidesToShow: 7,
+    slidesToScroll: 7,
     asNavFor: '.app_section_courses_slider_for',
-    focusOnSelect: true
+    focusOnSelect: true,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1160,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 3,
+          centerMode: true,
+          infinite: true,
+          centerPadding: '0px'
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 3,
+          centerMode: true,
+          infinite: true,
+          centerPadding: '0px'
+        }
+      }
+    ]
   });
+
 
   // Аккордеон
   $('.app_section_license_accordion_item_head').on('click', function() {
@@ -136,6 +238,7 @@ $(document).ready(function() {
 
     });
 
+
     // Активация вкладки
     $(this).children('.app_section_license_accordion_item_toggle')
       .children('img')
@@ -150,8 +253,10 @@ $(document).ready(function() {
 
   });
 
+
   // Триггер первой вкладки
   $($('.app_section_license_accordion_item')[0]).children().trigger('click');
+
 
   // Слайдер секции отзывы
   $('.app_section_feedback_slider').slick({
@@ -159,11 +264,21 @@ $(document).ready(function() {
     slidesToScroll: 1,
     prevArrow: '<div class="feedback_slider_prev_arrow prev_arrow"></div>',
     nextArrow: '<div class="feedback_slider_next_arrow next_arrow"></div>',
+    responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }
+  ]
   });
 
-  // Скрипты модалки
 
+  // Скрипты модалки
   function showModal(selectorBtn,selectorModal,selectorClose) {
+
     const btn = $(selectorBtn), modal =  $(selectorModal), close = $(selectorClose);
 
     btn.each(function (i, item) {
@@ -179,6 +294,7 @@ $(document).ready(function() {
 
         if(event.target){
           event.preventDefault();
+          closeMobileMenu();
         }
 
         modal.css({"display":"flex"});
